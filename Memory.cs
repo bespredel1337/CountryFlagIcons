232ac43f81c1c344aed2293a065d11d122092a64
bespredel1337 <124391377+bespredel1337@users.noreply.github.com>
2025-08-04 13:51:51 +0300

    upd sig

--- Memory.cs
+++ Memory.cs
@@ -6,5 +6,5 @@ namespace CountryFlagIcons;
 public static class Memory
 {
     public static readonly MemoryFunctionVoid<CCSPlayerController, IntPtr, IntPtr, IntPtr> CCSPlayerController_InventoryUpdateThink = 
-        new("48 8B BF 40 0A 00 00 E9");
+        new("55 48 89 E5 41 57 41 56 41 55 49 89 FD 41 54 53 48 81 EC ? ? ? ? E8 ? ? ? ? 4C 89 EF E8");
 }
